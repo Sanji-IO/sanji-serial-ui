8537abb9c70ada7af1df3663e20a57226bf871cd
Zack Yang <zack9433@gmail.com>
2016-09-12 18:08:54 +0800

    feat(component): add auth and response

--- app/component/serial.service.js
+++ app/component/serial.service.js
@@ -1,10 +1,23 @@
 import _ from 'lodash';
+import config from './component.resource.json';
 
 const $inject = ['$q', 'rest', 'exception', 'pathToRegexp', 'logger', '$filter'];
-const config = require('./component.resource.json');
 class SerialService {
   constructor(...injects) {
     SerialService.$inject.forEach((item, index) => this[item] = injects[index]);
+    this.restConfig = {
+      basePath: (__DEV__) ? __BASE_PATH__ : undefined
+    };
+    this.message = {
+      read: {
+        error: '[SerialService] Get data error.'
+      },
+      update: {
+        success: 'SERIAL_FORM_SAVE_SUCCESS',
+        error: '[SerialService] Update data error.'
+      }
+    };
+
     switch(config.get.type) {
     case 'collection':
       this.data = [];
@@ -46,13 +59,17 @@ class SerialService {
     }
   }
 
+  setResponseMsg(message) {
+    if (message) {
+      this.message = Object.assign(this.message, message);
+    }
+  }
+
   get() {
-    let toPath = this.pathToRegexp.compile(config.get.url);
+    const toPath = this.pathToRegexp.compile(config.get.url);
     // __BASE_PATH__ define in webpack
-    return this.rest.get(toPath(), (__DEV__) ? {basePath: __BASE_PATH__} : undefined)
-    .then(res => {
-      this.data = this._transform(res.data);
-    })
+    return this.rest.get(toPath(), this.restConfig)
+    .then(res => this.data = this._transform(res.data))
     .catch(err => {
       this.exception.catcher('[SerialService] Get data error.')(err);
       return this.$q.reject();
@@ -60,16 +77,16 @@ class SerialService {
   }
 
   update(data) {
-    let toPath = this.pathToRegexp.compile(config.put.url);
-    let path = (undefined !== data.content.id) ? toPath({id: data.content.id}) : toPath();
+    const toPath = this.pathToRegexp.compile(config.put.url);
+    const path = (undefined !== data.content.id) ? toPath({id: data.content.id}) : toPath();
     // __BASE_PATH__ define in webpack
-    return this.rest.put(path, data.content, data.formOptions.files, (__DEV__) ? {basePath:  __BASE_PATH__ } : undefined)
+    return this.rest.put(path, data.content, data.formOptions.files, this.restConfig)
     .then(res => {
-      this.logger.success(this.$filter('translate')('SERIAL_FORM_SAVE_SUCCESS'), res.data);
+      this.logger.success(this.$filter('translate')(this.message.update.success), res.data);
       return res.data;
     })
     .catch(err => {
-      this.exception.catcher('[SerialService] Update data error.')(err);
+      this.exception.catcher(this.$filter('translate')(this.message.update.error))(err);
       return this.$q.reject();
     });
   }
--- app/component/window.component.js
+++ app/component/window.component.js
@@ -1,4 +1,7 @@
 const SerialWindowComponent = {
+  bindings: {
+    onAuthorized: '&'
+  },
   template:`<sanji-window window-id="sanji-serial-ui"
               window-name="{{'SERIAL' | translate}}" show-loading-btn>
               <sanji-window-state default-state
@@ -7,12 +10,23 @@ const SerialWindowComponent = {
                 icon="info">
                 <sanji-serial-info-container></sanji-serial-info-container>
               </sanji-window-state>
-              <sanji-window-state
+              <sanji-window-state ng-if="::$ctrl.isAuthorized(['superadmin', 'admin'])"
                 state-name="sanji-form"
                 link-name="{{'SERIAL_FORM_SETTING' | translate}}"
                 icon="settings">
                 <sanji-serial-form-container></sanji-serial-form-container>
               </sanji-window-state>
-            </sanji-window>`
+            </sanji-window>`,
+  controller: class SerialWindowController {
+    constructor() {}
+
+    isAuthorized(roles) {
+      return this.onAuthorized({
+        $event: {
+          roles: roles
+        }
+      });
+    }
+  }
 };
 export default SerialWindowComponent;
