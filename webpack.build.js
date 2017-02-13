3006c613fd28f7dfb193d0233309af0681d7ed50
Zack Yang <zack9433@gmail.com>
2017-02-13 15:49:35 +0800

    refactor(core): modify core
    BREAKING CHANGE: upgrade core

--- webpack.build.js
+++ webpack.build.js
@@ -22,8 +22,8 @@ config.module.rules = [
   {
     test: /\.scss$/,
     loader: ExtractTextPlugin.extract({
-      notExtractLoader: 'style-loader',
-      loader: 'css-loader!postcss-loader!sass-loader?includePaths[]=' + bourbon
+      fallback: 'style-loader',
+      use: 'css-loader!postcss-loader!sass-loader?includePaths[]=' + bourbon
     })
   }
 ].concat(config.module.rules);
